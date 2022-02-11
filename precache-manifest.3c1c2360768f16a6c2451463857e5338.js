e59f1fbecc9a7b444ecb700a8ccf6f23e0169ff6
Solange ormeno <zol22@Solanges-MacBook-Air.local>
2022-02-11 17:20:01 -0500

    Updates

---
+++ precache-manifest.3c1c2360768f16a6c2451463857e5338.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8ba37131170a96d0df2d645261f64b",
+    "url": "/Slideshow_app/index.html"
+  },
+  {
+    "revision": "d9848c3cf77773905438",
+    "url": "/Slideshow_app/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "1d1342c38e6c05612619",
+    "url": "/Slideshow_app/static/css/main.e43691d7.chunk.css"
+  },
+  {
+    "revision": "d9848c3cf77773905438",
+    "url": "/Slideshow_app/static/js/4.0c0c6a88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Slideshow_app/static/js/4.0c0c6a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a785a24c9bbcf16c3d",
+    "url": "/Slideshow_app/static/js/5.dfb9c74f.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Slideshow_app/static/js/5.dfb9c74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db3ee45056257d4b9a4",
+    "url": "/Slideshow_app/static/js/6.53ebfd6b.chunk.js"
+  },
+  {
+    "revision": "1d1342c38e6c05612619",
+    "url": "/Slideshow_app/static/js/main.c92475ee.chunk.js"
+  },
+  {
+    "revision": "2c0fd7188b3f53a2aac2",
+    "url": "/Slideshow_app/static/js/runtime-main.fecd3db4.js"
+  },
+  {
+    "revision": "abd053df4ff34bf7f2a9",
+    "url": "/Slideshow_app/static/js/stencil-h8k-logo_2-entry-js.c6c63472.chunk.js"
+  },
+  {
+    "revision": "656dc23b9482aeb2c67b",
+    "url": "/Slideshow_app/static/js/stencil-polyfills-css-shim.aeec233e.chunk.js"
+  }
+]);
